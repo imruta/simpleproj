98a63f1d100458316ca935a860a8028ca289771a
Rams2005 <rameshwarpawar253@gmail.com>
2022-10-19 00:32:11 +0530

    Ram commit

--- SimpleProject/src/Test.java
+++ SimpleProject/src/Test.java
@@ -3,6 +3,8 @@ public class Test {
 
 	public static void main(String[] args) {
 System.out.println("amruta commit");
+System.out.println("Inside main method");
+System.out.println("Im Rameshwar");
 	}
 
 }
